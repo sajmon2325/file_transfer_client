f06d1ec434b58bd1935e01c52ed3af23f7aee6ab
Simon <sajmon2325@gmail.com>
2020-05-08 20:13:55 +0200

    Initial project setup

---
+++ src/main/java/com/opensourcedev/fileTransferClient/FileTransferClientApplication.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.fileTransferClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTransferClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTransferClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensourcedev/fileTransferClient/FileTransferClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.fileTransferClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTransferClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
